b4c56347976857b57fb6905cdc8d7ac20ff70249
Katie Shermer <katieshermer@gmail.com>
2017-06-02 08:39:56 -0600

    Created InvoiceItem and its test file

---
+++ lib/invoice_item.rb
@@ -0,0 +1,8 @@
+class InvoiceItem
+
+  def initialize(invoice_item_hash, invoice_item_repo)
+    @invoice_item_hash = invoice_item_hash
+    @invoice_item_repo = invoice_item_repo
+  end
+
+end
---
+++ test/invoice_item_test.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/invoice_item.rb'
+require 'pry'
+
+class InvoiceItemTest < Minitest::Test
+
+  def test_invoice_item_exists
+    invoice_item = InvoiceItem.new({:id => 1}, invoice_item_repo = nil)
+    assert_instance_of InvoiceItem, invoice_item
+  end
+
+end
